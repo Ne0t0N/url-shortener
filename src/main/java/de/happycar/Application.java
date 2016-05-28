8c8fd62b30b9db06674bfc068f6a373b490a90b3
NeotoN <airs.shmatko@yandex.ua>
2016-05-28 14:35:30 +0300

    Initial commit

---
+++ src/main/java/de/happycar/Application.java
@@ -0,0 +1,16 @@
+package de.happycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * User: Ne0t0N
+ * Date: 28.05.2016
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
